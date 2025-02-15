8bcdd9475005e9300f41728493d3df8824e4629f
Mohammad Issawi <2000issawi@gmail.com>
2025-02-15 12:51:19 +0300

    Implement creating a post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,7 +1,24 @@
-function create(req,res){
+const { validationResult } = require('express-validator')
+const Post = require('../models').Post
+
+function create(req, res) {
   res.render('create.ejs')
 }
 
-module.exports={
+async function store(req, res) {
+  const result = validationResult(req)
+  if (!result) {
+    res.render('create.ejs', { errors: result.error })
+  }
+  const post = await Post.create({
+    title: req.body.title,
+    date: req.body.date,
+    content: req.body.content
+  })
+  res.render('index.ejs')
+}
+
+module.exports = {
+  store,
   create
 }
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express=require('express')
-const PostController=require('./controllers/PostController')
+const express = require('express')
+const PostController = require('./controllers/PostController')
+const { body } = require('express-validator')
 
-const app=express()
+const app = express()
 
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
 
-app.get('/new',PostController.create)
+app.get('/new', PostController.create)
+app.post('/create', body("title").isString(), body("date").isDate(), body("content").isString(),
+  PostController.store)
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log('Listening on 3000')
 })
